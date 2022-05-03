79a00ca3b4c75394029ffc232035576cec1424f7
Khaled Hazime <hazimekhaled01@gmail.com>
2022-05-02 21:03:44 -0300

    creating switch-case menu

--- main.c
+++ main.c
@@ -1,49 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <time.h>
+
+#include "lib/utils.h"
 #include "lib/menu.h"
+#include "lib/dictionary.h"
 
 #define clrscr() printf("\e[1;1H\e[2J")
 
-#define menu_size 100
+#define window_size 100
 
-void draw_main_menu(){
-    draw_top_menu(menu_size);
-    draw_blank_line(menu_size);
-    center_text(menu_size, "The Hangman Game");
-    draw_blank_line(menu_size);
-    draw_vertical_line(1);
-    draw_horizontal_line(menu_size);
-    draw_vertical_line(1);
-    new_line();
-    draw_blank_line(menu_size);
-    center_text(menu_size, "Choose an option:");
-    draw_blank_line(menu_size);
-    draw_hangman(menu_size, 6);
-    center_text(menu_size, "1. Play the game");
-    center_text(menu_size, "2. Dictionary   ");
-    center_text(menu_size, "3. Credits      ");
-    center_text(menu_size, "4. Exit         ");
-    draw_blank_line(menu_size);
-    draw_bottom_menu(menu_size);
+void dictionary(){
+    int option;
+    draw_dictionary_menu(window_size);
+    while(1){
+        scanf("%d", &option);
+        switch(option){
+        case 1:
+            create_dictionary();
+            break;
+        case 2:
+            clrscr();
+            draw_dictionary_add_word_menu(window_size, 1, "");
+            dict_add_word(window_size);
+            break;
+        case 3:
+            // remove_word();
+            break;
+        case 4:
+            // search_word();
+            break;
+        case 5:
+            // main_menu();
+            break;
+        default:
+            center_text(window_size, "Invalid number, please choose one of the above options");
+            draw_space_line((window_size/2));
+            break;
+        }
+    }   
 }
-
-
 int main(){
     clrscr();
     int option = 0;
+    int dict = 0;
     while(1){
-        draw_main_menu();
+        draw_main_menu(window_size);
         scanf("%d", &option);
         switch(option){
             case 1:
                 clrscr();
-                
                 break;
+            case 2:
+                clrscr();
+                dictionary();
+                break;
+
         }
-        sleep(1000);
+        sleep(1000000);
     }
     
 }
