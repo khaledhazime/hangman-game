f02ebc4a8cbc1cb6f28c60e46f8e9dd97714a88d
Khaled Hazime <hazimekhaled01@gmail.com>
2022-05-18 17:14:28 -0300

    Added game loop and credits to menu

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
@@ -7,11 +8,13 @@
 #include "lib/utils.h"
 #include "lib/menu.h"
 #include "lib/dictionary.h"
+#include "lib/game.h"
 
 #define clrscr() printf("\e[1;1H\e[2J")
 
 #define window_size 100
 void main_menu();
+
 void dictionary(){
     int option;
     int menu_option;
@@ -84,10 +87,38 @@ void main_menu(){
         switch(option){
             case 1:
                 clrscr();
+                main_game_loop(window_size);
+                scanf(" %d", &option);
+                if(option == 1){
+                    clrscr();
+                    main_game_loop(window_size);
+                }
+                else if(option == 2){
+                    clrscr();
+                    main_menu();
+                }
+                else if(option == 3){
+                    clrscr();
+                    exit(0);
+                }
                 break;
             case 2:
                 clrscr();
                 dictionary();
+                break;
+            case 3:
+                clrscr();
+                draw_credits_menu(window_size);
+                clrscr();
+                break;
+            case 4:
+                clrscr();
+                exit(0);
+                break;
+
+            default:
+                center_text(window_size, "Invalid number, please choose one of the above options");
+                draw_space_line((window_size/2));
                 break;
 
         }
